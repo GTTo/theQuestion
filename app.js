aa14f57b68f8d23d2af052a13956d8ba98035a04
Cesar Bevia <cbevia@sistel.es>
2016-02-26 14:09:51 +0100

    Add a simple controller with scope var

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var app = angular.module('questionApp', []);
+
+app.controller('mainController', mainController);
+
+mainController.$inyect = ['$scope'];
+function mainController($scope) {
+    $scope.company = "Sistel";
+}
