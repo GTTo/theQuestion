bfe4eaa49a364a5256f7eddc40e60be6ceb3d02c
Cesar Bevia <cbevia@sistel.es>
2016-02-29 13:33:01 +0100

    Go question by question and check it has an answer

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ questionController.$inject = ['$scope'];
 function questionController($scope) {
     $scope.check = check;
     $scope.answer = answer;
+    $scope.questionNumber = 0;
+    $scope.notification = '';
 
     $scope.answers = [];
     $scope.questions = [
@@ -41,6 +43,32 @@ function questionController($scope) {
                     active: false
                 }
             ]
+        },
+        {
+            id: 2,
+            pregunta: '¿JavaScript mola?',
+            respuestas: [
+                {
+                    id: 1,
+                    text: 'Si, pero me vuelve loco',
+                    active: false
+                },
+                {
+                    id: 2,
+                    text: 'Si, pero no tengo ni idea',
+                    active: false
+                },
+                {
+                    id: 3,
+                    text: 'No, porque no tengo ni idea',
+                    active: false
+                },
+                {
+                    id: 4,
+                    text: 'javaque??',
+                    active: false
+                }
+            ]
         }
     ];
 
@@ -52,10 +80,22 @@ function questionController($scope) {
     };
 
     function answer(question) {
+        var hasResponse = false;
         angular.forEach(question.respuestas, function (value, key) {
             if (value.active) {
                 $scope.answers.push({ id: question.id, responseId: value.id });
+                hasResponse = true;
             }
         });
+        if (!hasResponse) {
+            $scope.notification = 'You have to answer the question!';
+            return;
+        }
+        //Next question
+        if ($scope.questionNumber < $scope.questions.length - 1) {
+            $scope.questionNumber++;
+        } else {
+            $scope.notification = 'At last question...';
+        }
     }
 };
